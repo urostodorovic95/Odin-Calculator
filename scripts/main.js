7339bea86d3ae10a376466f4f3328711b28b9da2
urostodorovic95 <150077866+urostodorovic95@users.noreply.github.com>
2024-01-13 21:26:44 +0100

    Solve zero division by returning prev result

--- scripts/main.js
+++ scripts/main.js
@@ -23,7 +23,11 @@ function subtract(a, b) {
 }
 
 function multiply(a, b) {
-  return a * b;
+  if (b !== 0) {
+    return a * b;
+  } else {
+    return operandOne;
+  }
 }
 
 function divide(a, b) {
